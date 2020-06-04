3e3d57a4a42a70680295e4d726fcea782f4f2ed8
KalebGz <kaleb.zeray@gmail.com>
2020-06-03 23:05:07 -0400

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,31 @@
 class Triangle
-  # write code here
+  attr_reader :s1, :s2, :s3
+
+  def initialize(s1, s2, s3)
+    @s1, @s2, @s3 = s1, s2, s3
+  end
+  
+  def kind
+    if (( (s1 + s2) <= s3 || (s2 + s3) <= s1 || (s3 + s1) <= s2) || !([s1, s2, s3].all? {|val| val > 0} ))
+      # begin
+          raise TriangleError
+    #   rescue TriangleError => error
+    #     puts error.message
+    # end
+    # else
+      end
+      (return :equilateral) if(s1 == s2 && s1 == s3)
+      return :isosceles if (s1 == s2 || s1 == s3 || s2 == s3)
+      :scalene
+    # end
+  end
+  
+  class TriangleError < StandardError
+
+    # def message
+    #   "Incorrect parameterw were given"
+    # end
+  
+  end
 end
+
